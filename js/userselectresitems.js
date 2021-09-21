b7b1f05012283e41b1a2d905af288c1a81637e6c
muhammad ameen <muhammadameenh1@gmail.com>
2021-09-22 03:25:21 +0500

    food delivery web App

--- js/userselectresitems.js
+++ js/userselectresitems.js
@@ -1,26 +1,43 @@
 const showResAllItems = () => {
     var id = localStorage.getItem("resid");
-    console.log(id);
+    let resName = document.getElementById('resName'), mainResName = document.getElementById('mainResName'), allResItems = document.getElementById('allResItems');
+    let items = '';
+    // FOr Getting Resturant Data
     db.collection("resturant").where("restaurantkey", "==", id).get()
         .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
-                // doc.data() is never undefined for query doc snapshots
-                console.log(doc.id, " => ", doc.data());
+                // console.log(doc.id, " => ", doc.data().name);
+                resName.innerHTML = `${doc.data().name} Restaurant Items`
+                mainResName.innerHTML = `${doc.data().name}`
             });
         })
         .catch((error) => {
             console.log("Error getting documents: ", error);
         });
 
-    // db.collection("items").where("key", "==", id).get()
-    //     .then((querySnapshot) => {
-    //         querySnapshot.forEach((doc) => {
-    //             // doc.data() is never undefined for query doc snapshots
-    //             console.log(doc.id, " => ", doc.data());
-    //         });
-    //     })
-    //     .catch((error) => {
-    //         console.log("Error getting documents: ", error);
-    //     });
+    // FOr Getting Resturant Items
+    db.collection("items").where("key", "==", id).get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+                items += `<div class="box-item pt-3">
+                            <div class="Item-nameNimage">
+                                <div class="itemname">
+                                    <p><b>${doc.data().itemname}</b><br>${doc.data().itemcategory}</p>
+                                    <p>Rs: ${doc.data().itemprice}</p>
+                                </div>
+                                <div class="itemimage">
+                                    <img src="./images/tikka.png" width="80px" alt="">
+                                    <i class="bi bi-plus-square-fill plus-item"></i>
+                                </div>
+                            </div>
+                        </div>`
+                allResItems.innerHTML=items;
+            });
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
 
 }
