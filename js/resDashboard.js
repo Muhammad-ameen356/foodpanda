c30286b4bafc2f87cfd9398819641ef8ba070e40
muhammad ameen <muhammadameenh1@gmail.com>
2021-10-09 02:22:47 +0500

    Location tracking added

--- js/resDashboard.js
+++ js/resDashboard.js
@@ -30,15 +30,14 @@ const pendingtab = () => {
                     var orders = [];
                     querySnapshot.forEach((doc) => {
                         orders.unshift(`<div>${doc.data().items}</div>` +
-                            `<b>Total: ${doc.data().total}</b>` +
+                            `<b id="itemtext">Total: ${doc.data().total}</b>` +
                             `<input type="hidden" id="pp${doc.data().orderid}" value="${doc.data().orderid}>" +
-                        <div><br></div>` +
+                            <div><br></div>` +
                             `<div class="pt-2 orderPerDet"><u><b class="">Order Person Detail</b></u></div>` +
-                            `<div class="row pt-2"><div class="col-md-4"><b>Name:</b> ${doc.data().customerName}</div><div class="col-md-4"><b>Phone #:</b> ${doc.data().customerPhone}</div><div class="col-md-4"><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div></div>` +
-                            `<div class="pt-1"><b>Address:</b> ${doc.data().customerAddress}</div>` +
-                            `<div><button class="btn btn-success no-radius" onclick="accept(${doc.data().orderid})">Accept</button> <button class="btn btn-danger no-radius" onclick="reject(${doc.data().orderid})">Reject</button></div><hr>`);
+                            `<div class="row pt-2"><div class="col-md-6"><div><b>Name:</b> ${doc.data().customerName}</div><div><b>Phone #:</b> ${doc.data().customerPhone}</div><div><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div><div><b>Address:</b> ${doc.data().customerAddress}</div><div><button class="btn btn-success no-radius" onclick="accept(${doc.data().orderid})">Accept</button> <button class="btn btn-danger no-radius" onclick="reject(${doc.data().orderid})">Reject</button></div></div> <div class="col-md-6"><iframe class="iframeClass" src="https://maps.google.co.uk/?q=${doc.data().latitude},${doc.data().longitude}&z=16&output=embed" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div></div>` +
+                            `<hr>`);
                     });
-                    pending.innerHTML = orders.join(" ");
+                    pending.innerHTML = orders.join(" ")
                     loader.style.display = "none"
                 }
             });
@@ -58,9 +57,8 @@ const acceptedtab = () => {
                         `<input type="hidden" id="pp${doc.data().orderid}" value="${doc.data().orderid}>" +
                         <div><br></div>` +
                         `<div class="pt-2 orderPerDet"><u><b class="">Order Person Detail</b></u></div>` +
-                        `<div class="row pt-2"><div class="col-md-4"><b>Name:</b> ${doc.data().customerName}</div><div class="col-md-4"><b>Phone #:</b> ${doc.data().customerPhone}</div><div class="col-md-4"><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div></div>` +
-                        `<div class="pt-1"><b>Address:</b> ${doc.data().customerAddress}</div>` +
-                        `<div><button class="btn btn-success no-radius" onclick="deliver(${doc.data().orderid})">Deliver</button><hr>`);
+                        `<div class="row pt-2"><div class="col-md-6"><div><b>Name:</b> ${doc.data().customerName}</div><div><b>Phone #:</b> ${doc.data().customerPhone}</div><div><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div><div><b>Address:</b> ${doc.data().customerAddress}</div><div><button class="btn btn-success no-radius" onclick="deliver(${doc.data().orderid})">Deliver</button></div></div> <div class="col-md-6"><iframe class="iframeClass" src="https://maps.google.co.uk/?q=${doc.data().latitude},${doc.data().longitude}&z=16&output=embed" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div></div>` +
+                        `<hr>`);
                 });
                 accepted.innerHTML = orders.join(" ")
             });
@@ -80,8 +78,8 @@ const deliveredtab = () => {
                         `<input type="hidden" id="pp${doc.data().orderid}" value="${doc.data().orderid}>" + 
                     <div><br></div>`+
                         `<div class="pt-2 orderPerDet"><u><b class="">Order Person Detail</b></u></div>` +
-                        `<div class="row pt-2"><div class="col-md-4"><b>Name:</b> ${doc.data().customerName}</div><div class="col-md-4"><b>Phone #:</b> ${doc.data().customerPhone}</div><div class="col-md-4"><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div></div>` +
-                        `<div class="pt-1"><b>Address:</b> ${doc.data().customerAddress}</div><hr>`);
+                        `<div class="row pt-2"><div class="col-md-6"><div><b>Name:</b> ${doc.data().customerName}</div><div><b>Phone #:</b> ${doc.data().customerPhone}</div><div><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div><div><b>Address:</b> ${doc.data().customerAddress}</div></div> <div class="col-md-6"><iframe class="iframeClass" src="https://maps.google.co.uk/?q=${doc.data().latitude},${doc.data().longitude}&z=16&output=embed" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div></div>` +
+                        `<hr>`);
                 });
                 // console.log(orders);
                 delivered.innerHTML = orders.join(" ")
@@ -100,8 +98,8 @@ const rejectedtab = () => {
                     orders.unshift(`<div>${doc.data().items}</div>` +
                         `<b>Total: ${doc.data().total} </b>` +
                         `<div class="pt-2 orderPerDet"><u><b class="">Order Person Detail</b></u></div>` +
-                        `<div class="row pt-2"><div class="col-md-4"><b>Name:</b> ${doc.data().customerName}</div><div class="col-md-4"><b>Phone #:</b> ${doc.data().customerPhone}</div><div class="col-md-4"><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div></div>` +
-                        `<div class="pt-1"><b>Address:</b> ${doc.data().customerAddress}</div><hr>`);
+                        `<div class="row pt-2"><div class="col-md-6"><div><b>Name:</b> ${doc.data().customerName}</div><div><b>Phone #:</b> ${doc.data().customerPhone}</div><div><b>Date: </b>${doc.data().date} <b>Time: </b>${doc.data().time}</div><div><b>Address:</b> ${doc.data().customerAddress}</div></div> <div class="col-md-6"><iframe class="iframeClass" src="https://maps.google.co.uk/?q=${doc.data().latitude},${doc.data().longitude}&z=16&output=embed" style="border:0;" allowfullscreen="" loading="lazy"></iframe></div></div>` +
+                        `<hr>`);
                 });
                 rejected.innerHTML = orders.join(" ")
             });
